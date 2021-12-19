c286ca380de84eced8b2c7052fcefd63a8548d1e
Iwata hayato <wakuwaku.shirokuma.works@gmail.com>
2021-12-19 10:23:55 +0900

    githubのリポジトリへの追加「dockerファイルも含め全て」

---
+++ project/src/pages/_slug/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <main class="main">
+    <h1 class="title">{{ title }}</h1>
+    <p class="publishedAt">{{ publishedAt }}</p>
+    <div class="post" v-html="body"></div>
+  </main>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  async asyncData({ params }) {
+    const { data } = await axios.get(
+      `https://iwata-test.microcms.io/api/v1/nuxt-blog1/${params.slug}`,
+      {
+        headers: { 'X-MICROCMS-API-KEY': 'a782da27f43144568aea7ca6a46a8b3d3022' }
+      }
+    )
+    return data
+  }
+}
+</script>
---
+++ project/src/pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <ul>
+    <li v-for="content in contents" :key="content.id">
+      <nuxt-link :to="`/${content.id}`">
+        {{ content.title }}
+      </nuxt-link>
+    </li>
+  </ul>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  async asyncData() {
+    const { data } = await axios.get(
+      // your-service-id部分は自分のサービスidに置き換えてください
+      'https://iwata-test.microcms.io/api/v1/nuxt-blog1',
+      {
+        // your-api-key部分は自分のapi-keyに置き換えてください
+        headers: { 'X-MICROCMS-API-KEY': 'a782da27f43144568aea7ca6a46a8b3d3022' }
+      }
+    )
+    return data
+  }
+}
+</script>
